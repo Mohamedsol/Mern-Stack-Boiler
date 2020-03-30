7755e8ba4bf578e07ddd8c3d290c0628c38c5c05
Mohamed Soltane <soltane.mohamed.achref@gmail.com>
2020-03-30 10:43:18 -0500

    install mongoose and connect to DB

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+// DB Config
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://Mohamed:Mohamed123@mernstack-qgev4.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 
 
+// Routes 
 app.get('/', (req, res) => {
     res.send('hello word')
 })
 
 
-app.listen(5000)
+const PORT =  5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
