59b47bf1f7a035caf20a7975b8c4fd2a2b7cf1b4
Mohamed Soltane <soltane.mohamed.achref@gmail.com>
2020-03-30 14:17:43 -0500

    Secure private info

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./model/user');
 
 
 // DB Config
 
+const db = require('./config/dev').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Mohamed:Mohamed123@mernstack-qgev4.mongodb.net/test?retryWrites=true&w=majority', 
-{
-    useNewUrlParser: true
-})
+mongoose.connect(db, {useNewUrlParser: true})
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-
+// body-parser
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // Routes 
-app.get('/', (req, res) => {
-    res.send('hello word')
+app.post('/api/user/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save( (err, userData) => {
+        if(err) return res.json ({success: false, err})
+        return res.status(200).json ({success: true })
+    })
 })
 
 
