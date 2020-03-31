1012e2997347665b789a693185409b561ac6ea2f
Mohamed Soltane <soltane.mohamed.achref@gmail.com>
2020-03-31 10:54:16 -0500

    heroku

--- server/index.js
+++ server/index.js
@@ -22,6 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
+app.get("/", (req, res) => {
+  res.json({ "hello": "I'm happy to deploy my application"})
+
+});
+
+
 app.get("/api/user/auth", auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
